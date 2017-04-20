f951cb9960e87ea093563eb50432820dd4a307b4
Keste <archana.keste@capgemini.com>
2017-04-20 12:07:01 +0530

    test case commite

---
+++ spring3-mvc-maven-xml-hello-world/src/test/java/com/mkyong/web/controller/TestCaseClass.java
@@ -0,0 +1,15 @@
+package com.mkyong.web.controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestCaseClass {
+
+	@Test
+	public void test() {
+		System.out.println("success");
+		
+	}
+
+}
